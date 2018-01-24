464dc7d862572d4f97390913a015d0029b26bacb
Daniel Phiri <malgamves@gmail.com>
2018-01-24 13:15:22 +0530

    Initial commit

---
+++ open_chrome.py
@@ -0,0 +1,92 @@
+import pyautogui as pg
+import time
+
+pg.PAUSE = 2
+pg.FAILSAFE = True
+
+pg.hotkey('win','d')
+time.sleep(5)
+
+
+pos = pg.locateOnScreen(r'C:\Users\malga\Desktop\Projects\PyAutoGui\img\chrome.png')
+
+x,y = pg.center(pos)
+
+pg.moveTo(x, y, duration = 2)
+
+pg.click(x, y, clicks = 2)
+
+pos = pg.locateOnScreen(r'C:\Users\malga\Desktop\Projects\PyAutoGui\img\sitetext.png')
+
+x,y = pg.center(pos)
+
+pg.moveTo(x, y, duration = 2)
+
+pg.click(x, y)
+
+pg.typewrite('http://180.211.119.163:8081/PUMIS/', interval = 0.5)
+
+pg.hotkey('enter')
+
+pg.hotkey('alt','space')
+
+pg.press('down', presses = 5)
+
+pg.hotkey('enter')
+
+pos = pg.locateOnScreen(r'C:\Users\malga\Desktop\Projects\PyAutoGui\img\student.png')
+
+x,y = pg.center(pos)
+
+pg.moveTo(x, y, duration = 2)
+
+pg.click(x, y)
+
+time.sleep(60)
+
+pg.typewrite('XXXXXXXX', interval = 0.5)
+
+pg.hotkey('tab')
+
+pg.typewrite('XXXX', interval = 0.5)
+
+pg.hotkey('enter')
+
+time.sleep(60)
+
+pos = pg.locateOnScreen(r'C:\Users\malga\Desktop\Projects\PyAutoGui\img\academic.png')
+
+x,y = pg.center(pos)
+
+pg.moveTo(x, y, duration = 2)
+
+pos = pg.locateOnScreen(r'C:\Users\malga\Desktop\Projects\PyAutoGui\img\attend.png')
+
+x,y = pg.center(pos)
+
+pg.moveTo(x, y, duration = 2)
+
+pg.click(x, y)
+
+time.sleep(20)
+
+pos = pg.locateOnScreen(r'C:\Users\malga\Desktop\Projects\PyAutoGui\img\hi.png')
+
+x,y = pg.center(pos)
+
+pg.moveTo(x, y, duration = 2)
+
+pos = pg.locateOnScreen(r'C:\Users\malga\Desktop\Projects\PyAutoGui\img\logout.png')
+
+x,y = pg.center(pos)
+
+pg.moveTo(x, y, duration = 2)
+
+pg.click(x, y)
+
+
+
+
+
+
+
