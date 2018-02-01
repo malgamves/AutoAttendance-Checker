15121e9b563de2b284f31f7d1ae4fb3671ecae22
Daniel Phiri <malgamves@gmail.com>
2018-02-01 12:20:21 +0530

    Fixes to open_chrome

--- open_chrome.py
+++ open_chrome.py
@@ -5,11 +5,11 @@ pg.PAUSE = 2
 pg.FAILSAFE = True
 
 pg.hotkey('win','d')
-time.sleep(2)
+time.sleep(.05)
 pg.hotkey('win')
 pg.typewrite('chrome', interval = 0.2)
 pg.hotkey('enter')
-pg.typewrite('http://180.211.119.163:8081/PUMIS/', interval = 0.1)
+pg.typewrite('http://10.0.0.4:8081/PUMIS/', interval = 0.1)
 pg.hotkey('enter')
 pg.hotkey('alt','space')
 pg.press('down', presses = 5)
@@ -18,12 +18,19 @@ pos = pg.locateOnScreen(r'C:\Users\malga\Desktop\Projects\PyAutoGui\img\student.
 x,y = pg.center(pos)
 pg.moveTo(x, y, duration = 2)
 pg.click(x, y)
-time.sleep(40)
+<<<<<<< HEAD
+time.sleep(3)
 pg.typewrite('150303105160', interval = 0.5)
 pg.hotkey('tab')
-pg.typewrite('785811', interval = 0.5)
+pg.typewrite('8581', interval = 0.5)
+=======
+time.sleep(40)
+pg.typewrite('XXXXXXXXXXXX', interval = 0.5)
+pg.hotkey('tab')
+pg.typewrite('XXXXXXX', interval = 0.5)
+>>>>>>> cd775571ae807706a826690fcea369727ed1976c
 pg.hotkey('enter')
-time.sleep(20)
+time.sleep(3)
 pos = pg.locateOnScreen(r'C:\Users\malga\Desktop\Projects\PyAutoGui\img\academic.png')
 x,y = pg.center(pos)
 pg.moveTo(x, y, duration = 2)
